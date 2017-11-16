4e3100ad95ccd1234e9ab63f3f71d72583f88a5e
scott coulton <scott.coulton@puppet.com>
2017-11-16 09:33:11 +0000

    removed modules

--- modules/helm/manifests/params.pp
+++
@@ -1,9 +0,0 @@
-class helm::params {
-  $env = [ 'HOME=/root', 'KUBECONFIG=/root/admin.conf']
-  $init = true
-  $install_path = '/usr/bin'
-  $path = [ '/bin','/usr/bin']
-  $service_account = 'tiller'
-  $tiller_namespace = 'kube-system'
-  $version = '2.5.1'
-}
--- modules/helm/spec/defines/repo_update_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'helm::repo_update', :type => :define do
-  let(:title) { 'helm repo update' }
-
-  context 'with default values for all params' do
-    it { is_expected.to contain_exec('helm repo update').with_command('helm repo update') }
-  end
-end
